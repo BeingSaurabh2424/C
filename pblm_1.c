03d7d40a461987574e05d4ee28969ee0f260d5e3
BeingSaurabh2424 <saurabhwadekar24@gmail.com>
2025-02-24 23:33:56 +0530

    Create pblm_1.c

---
+++ pblm_1.c
@@ -0,0 +1,24 @@
+//Write a program to input n numbers on command line argument and calculate maximum of them
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Usage: %s num1 num2 num3 ...\n", argv[0]);
+        return 1;
+    }
+
+    int max = atoi(argv[1]); // Initialize max with the first number
+
+    for (int i = 2; i < argc; i++) {
+        int num = atoi(argv[i]); // Convert argument to integer
+        if (num > max) {
+            max = num;
+        }
+    }
+
+    printf("Maximum number: %d\n", max);
+    return 0;
+}
+
